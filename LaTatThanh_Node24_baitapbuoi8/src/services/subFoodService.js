42f202aeed72e97bf92b02d1d85d1630766fe159
thanhla-amblique <thanh.la@amblique.com>
2022-10-29 21:54:21 +0700

    Update session 8

--- LaTatThanh_Node24_baitapbuoi8/src/services/subFoodService.js
+++ LaTatThanh_Node24_baitapbuoi8/src/services/subFoodService.js
@@ -7,6 +7,7 @@ const { Op } = require('sequelize');
 // Check sub food exist or not
 const subFoodExist = async (sub_id) => {
     if (Array.isArray(sub_id)) {
+        // If pass array sub_id
         let subFood = await SubFood.findAll({
             where: {
                 sub_id: {
@@ -17,6 +18,7 @@ const subFoodExist = async (sub_id) => {
 
         return subFood ? true : false;
     } else {
+        // If pass single sub_id
         let subFood = await SubFood.findOne({
             where: {
                 sub_id
@@ -27,39 +29,4 @@ const subFoodExist = async (sub_id) => {
     }
 }
 
-// Check sub food belong to food or not
-const checkSubFoodBelongFood = async (food_id, arr_sub_id) => {
-    // Get Food info includes sub food list
-    let food = await Food.findOne({
-        where: { food_id },
-        include: {
-            model: SubFood,
-            as: 'sub_foods',
-            attributes: ['sub_id']
-        }
-    });
-
-    // Convert sub food list to array include sub food id
-    let subFoods = food.sub_foods.map(subFood => subFood.sub_id);
-
-    let result = {
-        status: true,
-        sub_foods: []
-    };
-
-    // Check all sub foods belong to food or not.
-    // Return a sub_id not belong to food
-    // let checkSubFoods = arr_sub_id.every(sub_id => subFoods.includes(sub_id));
-    arr_sub_id.forEach(sub_id => {
-        let checkSubFood = subFoods.includes(sub_id);
-
-        if (!checkSubFood) {
-            result.status = false;
-            result.sub_foods.push(sub_id);
-        }
-    });
-
-    return result;
-}
-
-module.exports = { subFoodExist, checkSubFoodBelongFood }
+module.exports = { subFoodExist }
